58b625995950037e1f078715d52e37d1eb5c4d0e
Kristina <kristin.petrivska@gmail.com>
2024-07-12 18:26:15 -0600

    add solution code

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+const number = prompt('Enter three digit number.');
+const absIntNumber = parseInt(Math.abs(number));
+
+//validation
+if (number === null) {
+    alert('You canceled the input');
+} else if (!number.trim) {
+    alert('Input cannot be empty!');
+} else if (isNaN(number)) {
+    alert('Input must consist of digits only!')
+} else if (absIntNumber.toString().length !== 3) {
+    alert('Number must has three digits.');
+}else{
+    //is a 3digit number
+    const numObject = absIntNumber.toString().split("");
+    const sameThreeDigits = +numObject[0] === +numObject[1] && +numObject[1] === +numObject[2];
+    const sameTwoDigits = +numObject[0] === +numObject[1] 
+    || +numObject[1] === +numObject[2] 
+    || +numObject[0] === +numObject[2];
+    if(sameThreeDigits){
+        console.log("All digits are same.")
+    }else if(sameTwoDigits){
+        console.log("There are same digits in the number.")
+    }else{
+        console.log("All digits are different.") 
+    }
+}
+   
+
+
